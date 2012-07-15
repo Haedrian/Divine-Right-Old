815647c48dcf2daa8cb40034c3745cf852adaaa9
Vistarino <byronmagri@gmail.com>
2012-07-15 18:20:11 +0200

    The global map item object. So far only carried information on elevation of map, will also have information on climate and weather.

---
+++ src/objects/common/GlobalMapItem.java
@@ -0,0 +1,24 @@
+package objects.common;
+
+public class GlobalMapItem extends MapItem {
+
+	protected int elevation;
+	//protected ClimateType climatetype;
+	//protected int climate;
+	
+	
+	/**
+	 * @return the elevation of global maptile
+	 */
+	public int getElevation() {
+		return elevation;
+	}
+	/**
+	 * @param elevation the elevation to set
+	 */
+	
+	public void setElevation(int elevation) {
+		this.elevation = elevation;
+	} 
+	
+}

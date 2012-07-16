2efe286908a01e5b12fc1070b685211dd4311c62
Vistarino <byronmagri@gmail.com>
2012-07-16 17:30:00 +0200

    Added regions, need to do smoothening next.

--- src/generators/GlobalMapGenerator.java
+++ src/generators/GlobalMapGenerator.java
@@ -1,17 +1,27 @@
 package generators;
 
 import objects.common.GlobalMapItem;
+import objects.common.Region;
+
+import java.util.Random;
+import java.util.ArrayList;
 
 public class GlobalMapGenerator{ 
 
-protected GlobalMapItem[][] globalmap = new GlobalMapItem[1000][1000];
+protected final int WORLDSIZE = 1000;
+protected GlobalMapItem[][] globalmap = new GlobalMapItem[WORLDSIZE][WORLDSIZE];
+protected Random random = new Random();
+protected Region[] worldregion;
+protected int regionflag;
 
 
 /**
- * Inialises world map with an elevation of 100 everywhere. The elevation of the edges is set to -100
+ * Inialises world map The elevation of the edges is set to -100, regions are initialized and populated
  * @author Byron
  */
-public GlobalMapGenerator (){
+public GlobalMapGenerator (int regionnumber){
+	regionnumber = 1000;
+	worldregion = new Region[regionnumber +1];
 	for (GlobalMapItem[] mapItemRow : globalmap )
 	{
 		for (GlobalMapItem mapItem : mapItemRow)
@@ -22,17 +32,64 @@ public GlobalMapGenerator (){
 		}
 		
 	}
-
-	for (int i=0; i < globalmap[0].length; i++)
-	{
-		globalmap[0][i].setElevation(-100);
-		globalmap[globalmap[0].length-1][i].setElevation(-100);
-		globalmap[i][0].setElevation(-100);
-		globalmap[i][globalmap[i].length-1].setElevation(-100);
+   //setting coordinates for GlobalMapItems
+	for (int i = 0; i < WORLDSIZE; i++){
+		for (int j = 0; j < WORLDSIZE; j++){
+			globalmap[i][j].getPosition().setX(i);
+			globalmap[i][j].getPosition().setY(j);
+			globalmap[i][j].getPosition().setZ(0);
+		}
 	}
-
+	//setting region 0, this is the boarder of the world map
+	for (int i=0; i < WORLDSIZE; i++)
+	{
+		worldregion[0].addToRegion(globalmap[0][i]);
+		worldregion[0].addToRegion(globalmap[i][0]);
+		worldregion[0].addToRegion(globalmap[WORLDSIZE - 1][i]);
+		worldregion[0].addToRegion(globalmap[i][WORLDSIZE-1]);
+		worldregion[0].getCenter().setX(-1);
+		worldregion[0].getCenter().setY(-1);
+		worldregion[0].getCenter().setZ(-1);
+	}
+	    
+    //setting region 0s properties. These will correspond to deep oceans surrounding the game map
+	for (int i = 0; i < worldregion[0].getRegion().size(); i++ )
+	{
+    	worldregion[0].getRegion().get(i).setElevation(-100);
+    }
+    
+	//setting region centers. Perhaps number of regions will be user defined? working with 1000, region 0 does not count;
 	
-	
-}
-
+	for (int i = 1; i <= regionnumber; i++)
+	{
+	    worldregion[i].getCenter().setX(random.nextInt(800)+100);
+	    worldregion[i].getCenter().setY(random.nextInt(800)+100);
+	    worldregion[i].getCenter().setZ(0);
+	}
+    //populating regions
+	for (int i = 0; i < WORLDSIZE; i++){
+		for(int j = 0; j < WORLDSIZE; j++){
+			if (!globalmap[i][j].getIsInRegion()){
+				double mindistance = WORLDSIZE^4;
+				for (int iter = 1; i<= regionnumber; iter++){
+					double distance = globalmap[i][j].getPosition().displacement(worldregion[iter].getCenter());
+					if (distance <= mindistance){
+						mindistance = distance;
+						regionflag = iter;
+					}
+				 
+				}
+			}
+		  worldregion[regionflag].addToRegion(globalmap[i][j]);
+		}
+	}
+    //setting random elevations for different regions
+	for (int iter = 0; iter <= regionnumber; iter++){
+		int elev = random.nextInt(200) - 100;
+	    for (int i = 0; i < worldregion[iter].getRegion().size(); i++){
+	    	worldregion[iter].getRegion().get(i).setElevation(elev);
+	    }
+     }
+   //TODO Smoothing
+  }
 }

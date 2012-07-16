b5bcf3953abeca2e3089e10df7fbb936e35f8260
Vistarino <byronmagri@gmail.com>
2012-07-16 18:59:56 +0200

    testing generator

--- src/system/runnable/YuryTest.java
+++ src/system/runnable/YuryTest.java
@@ -1,5 +1,8 @@
 package system.runnable;
 
+import generators.GlobalMapGenerator;
+import java.io.*;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Yury
@@ -8,6 +11,31 @@ package system.runnable;
  */
 public class YuryTest {
     public static void main(String[] args) {
-        //do nothing for now
-    }
+      try{ 
+    	System.out.println("Generating World");
+    	GlobalMapGenerator worldmap = new GlobalMapGenerator(1000);
+    	System.out.println("Writing File");
+        FileWriter fstream = new FileWriter("out.txt");
+        BufferedWriter out = new BufferedWriter(fstream);
+        for (int i = 0; i < 1000; i++){
+        	out.write("\n");
+        	for (int j = 0; j < 1000; j++){
+        		int elev = worldmap.getGlobalMapItem(i, j).getElevation();
+        		if (elev >= 60) {out.write("^");}
+        		else if (elev >= 1) {out.write("*");}
+        		else if (elev <= 0) {out.write("~");}
+        		}
+        		
+        	}
+        out.close();
+        System.out.println("File Done");
+           }catch (Exception e){//Catch exception if any
+        	   System.err.println("Error: " + e.getMessage());
+           }
+        }
+    
+   
 }
+
+
+
